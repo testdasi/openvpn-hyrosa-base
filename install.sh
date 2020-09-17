c612ad6c2c9e81cf8f14ea7bb943c585a7bad8a1
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-17 13:53:33 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-### openvpn, nftables, stubby, tinyproxy, dante, sabnzbdplus, nzbhydra2 are in the base image ###
+### openvpn, nftables, stubby, tinyproxy, dante, sabnzbdplus, nzbhydra2, curl are in the base image ###
 
-apt-get -y update
+# install rtorrent with screen (to send it to background)
+apt-get -y update \
+    && apt-get -y install rtorrent screen
+    
+# install additional packages to build flood
+apt-get -y install build-essential git
+# install nodejs to run flood
+curl -sL https://deb.nodesource.com/setup_lts.x | bash - \
+    && apt-get -y install -y nodejs
+# build flood
+cd /app \
+    && npm update \
+    && git clone https://github.com/jfurrow/flood.git \
+    && cd flood \
+    && cp config.template.js config.js \
+    && npm install \
+    && npm install -g node-gyp \
+    && npm run build
+
 
 # clean up
+apt
 #apt-get -y autoremove \
 #    && apt-get -y autoclean \
 #    && apt-get -y clean \
